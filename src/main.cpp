6c796cdc26e64120a8ecc77eaedafd9852e74ec8
Otho Teixeira <otho.tk@hotmail.com>
2019-09-23 19:01:08 -0300

    Initial program
    Opens a file given by command line execution, and reads its content

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+
+using namespace std;
+
+int main(int argc,char* argv[])
+{
+  char asm_line_code [100];
+
+  // Source codes are locates in the directory /sources/asm_codes/
+  // outside current /exec/ directory(where the program is executed)
+  string string_source_code_name = "../src/asm_codes/";
+
+  // Source code name input at the terminal execution
+  // concatenated with directory
+  string_source_code_name += argv[1];
+
+  // Convert source_code_name stringvariable to char type
+  char char_source_code_name[string_source_code_name.size() + 1];
+  strcpy(char_source_code_name, string_source_code_name.c_str());
+
+  // Open the chosen source code file
+  FILE* source_code = fopen(char_source_code_name, "r");
+  if (source_code == NULL) perror ("Error opening file");
+  else
+   {
+     // Until reach end of file
+     while (!feof (source_code) )
+     {
+       // Gets 100 characters from the source code line
+       if ( fgets (asm_line_code , 100 , source_code) == NULL ) break;
+     }
+     fclose (source_code);
+   }
+
+  return 0;
+}
