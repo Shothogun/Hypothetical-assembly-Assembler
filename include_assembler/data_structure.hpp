c31349d71dda7ab360eac24fdab7fe449d81d13a
daniloinacio <daniloinacio@aluno.unb.br>
2019-10-20 10:41:15 -0300

    Inclusão de alguns comentários

--- include_assembler/data_structure.hpp
+++ include_assembler/data_structure.hpp
@@ -7,177 +7,487 @@
 #include <list>
 using namespace std;
 
-// This class creates a generic table with a search method 
+/*! Table class
+ * 
+ * This class creates a generic table with a search method 
+*/
 class table{
 protected:
-    static const int T_SIZE = 2;
-    map<string, int[T_SIZE]> table;
-    map<string, int[T_SIZE]>::iterator itr;
-    
+  /*! T_SIZE
+   *
+   * Constant that indicates the number of columns in the table
+  */
+  static const int T_SIZE = 2;
+  /*! table
+   *
+   * Table structure
+  */
+  map<string, int[T_SIZE]> table;
+  /*! itr
+   *
+   * Iterator of table structure
+  */
+  map<string, int[T_SIZE]>::iterator itr;
+
 public:
-    // Tells whether an identifier is present in the table or not
-    bool search(string identifier);
+/*! search(string identifier)
+ *    
+ * Tells whether an identifier is present in the table or not
+*/ 
+  bool search(string identifier);
 };
 
-// This class creates a instruction table with methods for acess number of operands and
-// opcode for each instruction in table
+/*!
+ *
+ * This class creates a instruction table with methods for acess number of operands and
+ * opcode for each instruction in table
+*/
 class instruction_table : public table{
-private:
+  private:
+    /*! OPCODE
+     *
+     * Constant indicating opcode column number in table
+    */
     static const int OPCODE = 0;
+    /*! N_OPERANDS
+     *
+     * Constant indicating operands number column number in table
+    */
     static const int N_OPERANDS = 1;
+    /*! ERROR
+     *
+     * Constatant indicating a error
+    */
     static const int ERROR = -1;
-public:
-    //  Initializes the instruction table
+  public:
+    /*! instruction_table()
+     *
+     *  Initializes the instruction table
+    */
     instruction_table();
-    // Returns number of operands of instruction 
+    /*! get_num_operands(string identifier)
+     *
+     * Returns number of operands of instruction 
+    */
     int get_num_operands(string identifier);
-    // Return instruction opcode
+    /*! get_opcode(string identifier)
+     *
+     * Return instruction opcode
+    */
     int get_opcode(string identifier);
 };
-
-// This class creates a symbol table with methods for accessing the value, definition,
-// and address of the undefined symbol list for each symbol in table
+/*!
+ *
+ * This class creates a symbol table with methods for accessing the value, definition,
+ * and address of the undefined symbol list for each symbol in table
+*/
 class symbol_table : public table{
-private:
+  private:
+    /*! T_SIZE
+     *
+     * Constant that indicates the number of columns in the table
+    */
     static const int T_SIZE = 3;
+    /*! VALUE
+     *
+     * Constant indicating value column number in table
+    */
     static const int VALUE = 0;
+    /*! DEF
+     *
+     * Constant indicating definition column number in table
+    */
     static const int DEF = 1;
+    /*! L_ADDRESS
+     *
+     * Constant indicating list address column number in table
+    */
     static const int L_ADDRESS = 2;
+    /*! ERROR
+     *
+     * Constatant indicating a error
+    */
     static const int ERROR = -1;
+    /*! table
+     *
+     * Table structure
+    */
     map<string, int[T_SIZE]> table;
+    /*! itr
+     *
+     * Iterator of table structure
+    */
     map<string, int[T_SIZE]>::iterator itr;
 
-public:
-
+  public:
+    /*! set_value(string identifier, int value)
+     *
+     * Sets the value of a symbol
+    */
     void set_value(string identifier, int value){
-        this->table[identifier][VALUE] = value;
+      this->table[identifier][VALUE] = value;
     }
-
+    /*! set_definition(string identifier, bool value)
+     * 
+     * Sets the definition of a symbol
+    */
     void set_definition(string identifier, bool value){
-        this->table[identifier][DEF] = (int)value;
+      this->table[identifier][DEF] = (int)value;
     }
-
+    /*! set_list_address(string identifier, int address)
+     *
+     * Sets the list address of a symbol
+    */
     void set_list_address(string identifier, int address){
-        this->table[identifier][L_ADDRESS] = address;
+      this->table[identifier][L_ADDRESS] = address;
     }
-
+    /*! get_value(string identifier)
+     *
+     * Returns the value of a symbol.
+    */
     int get_value(string identifier);
+    /*! get_definition(string identifier)
+     *
+     * Returns the definition of a symbol.
+    */
     bool get_definition(string identifier);
+    /*! get_list_address(string identifier)
+     *
+     * Returns the list address of a symbol.
+    */
     int get_list_address(string identifier);
 
-    //! PrintTable();
-    // A debug function
+    /*! PrintTable();
+     * A debug function
+     *
+    */ 
     void PrintTable();
 
-    // Tells whether an identifier is present in the table or not
+    /*! search(string identifier)
+     *
+     * Tells whether an identifier is present in the table or not
+    */
     bool search(string identifier);
 
     vector<string> search_not_defined();
 };
 
-// This class stores information about an occurrence of a label
+/*!
+ *
+ * This class stores information about an occurrence of a label
+*/
 class label_occurrence{
-private:
+  private:
+    /*! instruction_operator
+     *
+     * Stores the instruction operator using the label
+    */
     string instruction_operator;
+    /*! code_line
+     *
+     * Stores the line of code where the label is used
+    */
     string code_line;
+    /*! line_number
+     *
+     * Stores the line number where the label is used
+    */
     int line_number;
+    /*! label
+     *
+     * Stores the label;
+    */
     string label;
-public:
+  public:
+    /*! label_occurrence(string label, string instruction_operator, string code_line, int line_number)
+     *
+     * label_occurence class constructor
+    */
     label_occurrence(string label, string instruction_operator, string code_line, int line_number);
-
+    /*! set_instruction_operator(string instruction_operator
+     *
+     * Sets the instruction operator using the label
+    */
     void set_instruction_operator(string instruction_operator){
-        this->instruction_operator = instruction_operator;
+      this->instruction_operator = instruction_operator;
     }
+    /*! set_label(string label)
+     *
+     * Sets the label;
+    */
     void set_label(string label){
-        this->label = label;
-    }
+      this->label = label;
+    } 
+    /*! set_code_line(string code_line)
+     *
+     * Sets the line of code where the label is used
+    */
     void set_code_line(string code_line){
-        this->code_line = code_line;
+      this->code_line = code_line;
     }
+    /*! set_line_number(int line_number)
+     *
+     * Sets the line number where the label is used
+    */
     void set_line_number(int line_number){
-        this->line_number = line_number;
+      this->line_number = line_number;
     }
+    /*! get_instruction_operator()
+     *
+     * Return the instruction operator using the label
+    */
     string get_instruction_operator(){
-        return this->instruction_operator;
+      return this->instruction_operator;
     }
+    /*! get_label()
+     *
+     * Return the label
+    */
     string get_label(){
-        return this->label;
+      return this->label;
     }
+    /*! get_code_line()
+     *
+     * Return the line of code where the label is used
+    */
     string get_code_line(){
-        return this->code_line;
+      return this->code_line;
     }
+    /*! get_line_number()
+     *
+     * Return the line number where the label is used
+    */
     int get_line_number(){
-        return this->line_number;
+      return this->line_number;
     }
 };
 
-//This class implements the error type and contains the line where the error occurred,
-// a classification (lexical, syntactic and semantic), a error code and the description.
+/*!
+ *
+ * This class implements the error type and contains the line where the error occurred,
+ * a classification (lexical, syntactic and semantic), a error code and the description.
+*/
 class error{
-private:
+  private:
+    /*! ERROR_TYPE_MASK
+     *
+     * Mask that lets you determine the type of error from your error code
+    */
     static const int ERROR_TYPE_MASK = 1000;
+    /*! error_line
+     *
+     * Stores error line string
+    */
     string error_line;
+    /*! error_line_number
+     *
+     * Stores error line number
+    */
     int error_line_number;
+    /*! error_code
+     *
+     * Stores error code
+    */
     int error_code;
+    /*! error_type
+     *
+     * Stores error type 
+    */
     int error_type;
+    /*! error_details
+     *
+     * Stores strings matching error details
+    */
     static map<int, string> error_details;
 
-public:
-
+  public:
+    /*! LEXICAL
+     *
+     * Constant indicating lexical type
+    */
     static const int LEXICAL = 0;
+    /*! SYNTACTIC
+     *
+     * Constant indicating syntactic type
+    */
     static const int SYNTACTIC = 1;
+    /*! SEMANTIC
+     *
+     * Constant indicating semantic type
+    */
     static const int SEMANTIC = 2;
+    /*! error_types
+     *
+     * Stores strings matching error type
+    */
     static map<int, string> error_types;
-
+    /*! error_00
+     *
+     * Code for the error "Declaração e rótulo ausente."
+    */
     static const int error_00 = 1000; // syntactic
+    /*! error_01
+     *
+     * Code for the error "Declaração e rótulo repetido."
+    */
     static const int error_01 = 2000; // semantic
+    /*! error_02
+     *
+     * Code for the error "Pulo para rótulo inválido."
+    */
     static const int error_02 = 2001; // semantic
+    /*! error_03
+     *
+     * Code for the error "Pulo para seção errada."
+    */
     static const int error_03 = 2002; // semantic
+    /*! error_04
+     *
+     * Code for the error "Diretiva inválida."
+    */
     static const int error_04 = 1001; // syntactic
+    /*! error_05
+     *
+     * Code for the error "Instrução inválida."
+    */
     static const int error_05 = 1002; // syntactic
+    /*! error_06
+     *
+     * Code for the error "Diretiva ou instrução na seção errada."
+    */
     static const int error_06 = 2003; // semantic
+    /*! error_07
+     *
+     * Code for the error "Divisão por zero."
+    */
     static const int error_07 = 2004; // semantic
+    /*! error_08
+     *
+     * Code for the error "Instrução com a quantidade de operandos inválida."
+    */
     static const int error_08 = 1003; // syntactic
+    /*! error_09
+     *
+     * Code for the error "Instrução com o tipo de operando inválido."
+    */
     static const int error_09 = 1004; // syntactic
+    /*! error_10
+     *
+     * Code for the error "Token inválido."
+    */
     static const int error_10 = 0000; // lexical
+    /*! error_11
+     *
+     * Code for the error "Dois rótulos na mesma linha."
+    */
     static const int error_11 = 1005; // syntactic
+    /*! error_12
+     *
+     * Code for the error "Seção TEXT faltante."
+    */
     static const int error_12 = 2005; // semantic
+    /*! error_13
+     *
+     * Code for the error "Seção inválida."
+    */
     static const int error_13 = 1006; // syntactic
+    /*! error_14
+     *
+     * Code for the error  "Tipo de argumento inválido."
+    */
     static const int error_14 = 1007; // syntactic
+    /*! error_15
+     *
+     * Code for the error "Modificação de um valor constante."
+    */
     static const int error_15 = 2006; // semantic
+    /*! error_16
+     *
+     * Code for the error "Acesso a posição não reservada pelo SPACE."
+    */
     static const int error_16 = 2007; // semantic
 
+    /*! error(string error_line, int error_line_number, int error_code)
+     *
+     * error class constructor
+    */    
     error(string error_line, int error_line_number, int error_code);
+    /*! get_error_line_number()
+     *
+     * Returns the number of the line on which the error occurred.
+    */    
     int get_error_line_number(){
-        return this->error_line_number;
+      return this->error_line_number;
     }
+    /*! get_error_code()
+     *
+     * Returns the number of the line on which the error occurred.
+    */    
     int get_error_code(){
-        return this->error_code;
+      return this->error_code;
     }
+    /*! get_error_line()
+     *
+     * Returns the line on which the error occurred.
+    */    
     string get_error_line(){
-        return this->error_line;
+      return this->error_line;
     }
+    /*! get_error_type()
+     *
+     * Returns the type of error occurred
+    */    
     int get_error_type(){
-        return this->error_type;
+      return this->error_type;
     }
+    /*! get_error_details()
+     *
+     * Returns details of error occurred
+    */    
     string get_error_details(){
-        return this->error_details[this->error_code];
+      return this->error_details[this->error_code];
     }
 };
 
-// This class implements an error log that stores a set of errors 
-// and allows them to be displayed simply or in detail.
-
+/*!
+ *
+ * This class implements an error log that stores a set of errors 
+ * and allows them to be displayed simply or in detail.
+*/
 class error_log{
-private:
+  private:
+    /*! error_list
+     *
+     * error log structure
+    */
     list<error> error_list;
+    /*!  itr
+     *
+     * Iterator of error log structure
+    */
     list<error>::iterator itr;
-public:
+  public:
+    /*! DETAILED
+     *
+     * Constant indicating detailed display option
+    */    
     static const int DETAILED = 1;
+    /*! SIMPLE
+     *
+     * Constant indicating simple display option
+    */
     static const int SIMPLE = 0;
+    /*! include_error(error error)
+     *
+     * Insert object of error class into error log
+    */
     void include_error(error error){
-        this->error_list.push_back(error);
+      this->error_list.push_back(error);
     }
+    /*! display(int option)
+     *
+     * Display all errors in error log detailed or simple form
+    */    
     void display(int option);
 };
 #endif
--- src_assembler/data_structure.cpp
+++ src_assembler/data_structure.cpp
@@ -10,192 +10,192 @@ using namespace std;
 // CLASS TABLE
 
 bool table::search(string identifier){
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return true;
-    }
-    else{
-        return false;
-    }
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 
 // CLASS INSTRUCTION_TABLE
 
 instruction_table::instruction_table(){
-    this->table["ADD"][OPCODE]  = 1;
-    this->table["ADD"][N_OPERANDS]  = 1;
-    this->table["SUB"][OPCODE]  = 2;
-    this->table["SUB"][N_OPERANDS]  = 1;
-    this->table["MULT"][OPCODE]  = 3;
-    this->table["MULT"][N_OPERANDS]  = 1;
-    this->table["DIV"][OPCODE]  = 4;
-    this->table["DIV"][N_OPERANDS]  = 1;
-    this->table["JMP"][OPCODE]  = 5;
-    this->table["JMP"][N_OPERANDS]  = 1;
-    this->table["JMPN"][OPCODE]  = 6;
-    this->table["JMPN"][N_OPERANDS]  = 1;
-    this->table["JMPP"][OPCODE]  = 7;
-    this->table["JMPP"][N_OPERANDS]  = 1;
-    this->table["JMPZ"][OPCODE]  = 8;
-    this->table["JMPZ"][N_OPERANDS]  = 1;
-    this->table["COPY"][OPCODE]  = 9;
-    this->table["COPY"][N_OPERANDS]  = 2;
-    this->table["LOAD"][OPCODE]  = 10;
-    this->table["LOAD"][N_OPERANDS]  = 1;
-    this->table["STORE"][OPCODE]  = 11;
-    this->table["STORE"][N_OPERANDS]  = 1;
-    this->table["INPUT"][OPCODE]  = 12;
-    this->table["INPUT"][N_OPERANDS]  = 1;
-    this->table["OUTPUT"][OPCODE]  = 13;
-    this->table["OUTPUT"][N_OPERANDS]  = 1;
-    this->table["STOP"][OPCODE]  = 14;
-    this->table["STOP"][N_OPERANDS]  = 0;
+  this->table["ADD"][OPCODE]  = 1;
+  this->table["ADD"][N_OPERANDS]  = 1;
+  this->table["SUB"][OPCODE]  = 2;
+  this->table["SUB"][N_OPERANDS]  = 1;
+  this->table["MULT"][OPCODE]  = 3;
+  this->table["MULT"][N_OPERANDS]  = 1;
+  this->table["DIV"][OPCODE]  = 4;
+  this->table["DIV"][N_OPERANDS]  = 1;
+  this->table["JMP"][OPCODE]  = 5;
+  this->table["JMP"][N_OPERANDS]  = 1;
+  this->table["JMPN"][OPCODE]  = 6;
+  this->table["JMPN"][N_OPERANDS]  = 1;
+  this->table["JMPP"][OPCODE]  = 7;
+  this->table["JMPP"][N_OPERANDS]  = 1;
+  this->table["JMPZ"][OPCODE]  = 8;
+  this->table["JMPZ"][N_OPERANDS]  = 1;
+  this->table["COPY"][OPCODE]  = 9;
+  this->table["COPY"][N_OPERANDS]  = 2;
+  this->table["LOAD"][OPCODE]  = 10;
+  this->table["LOAD"][N_OPERANDS]  = 1;
+  this->table["STORE"][OPCODE]  = 11;
+  this->table["STORE"][N_OPERANDS]  = 1;
+  this->table["INPUT"][OPCODE]  = 12;
+  this->table["INPUT"][N_OPERANDS]  = 1;
+  this->table["OUTPUT"][OPCODE]  = 13;
+  this->table["OUTPUT"][N_OPERANDS]  = 1;
+  this->table["STOP"][OPCODE]  = 14;
+  this->table["STOP"][N_OPERANDS]  = 0;
 
 }
 
 
 int instruction_table::get_num_operands(string identifier){
-    // Check if instruction is in table
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return table[identifier][N_OPERANDS];
-    }
-    else{
-        return ERROR;
-    }
+  // Check if instruction is in table
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return table[identifier][N_OPERANDS];
+  }
+  else{
+    return ERROR;
+  }
 }
 
 int instruction_table::get_opcode(string identifier){
-    // Check if instruction is in table
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return table[identifier][OPCODE];
-    }
-    else{
-        return ERROR;
-    }
+  // Check if instruction is in table
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return table[identifier][OPCODE];
+  }
+  else{
+    return ERROR;
+  }
 }
 
 // CLASS SYMBOL_TABLE
 bool symbol_table::search(string identifier){
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return true;
-    }
-    else{
-        return false;
-    }
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 int symbol_table::get_value(string identifier){
-    // Check if symbol is in table
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return table[identifier][VALUE];
-    }
-    else{
-        return ERROR;
-    }
+  // Check if symbol is in table
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return table[identifier][VALUE];
+  }
+  else{
+    return ERROR;
+  }
 }
 
 bool symbol_table::get_definition(string identifier){
-    // Check if symbol is in table
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return (bool)table[identifier][DEF];
-    }
-    else{
-        return false;
-    }
+  // Check if symbol is in table
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return (bool)table[identifier][DEF];
+  }
+  else{
+    return false;
+  }
 }
 
 int symbol_table::get_list_address(string identifier){
-    // Check if symbol is in table
-    itr = this->table.find(identifier);
-    if(itr != this->table.end()){
-        return table[identifier][L_ADDRESS];
-    }
-    else{
-        return ERROR;
-    }
+  // Check if symbol is in table
+  itr = this->table.find(identifier);
+  if(itr != this->table.end()){
+    return table[identifier][L_ADDRESS];
+  }
+  else{
+    return ERROR;
+  }
 }
 
 void symbol_table::PrintTable(){
-    map<string, int[T_SIZE]> table;
-    
-    for (map<string, int[T_SIZE]>::iterator it= this->table.begin(); it!=this->table.end(); ++it)
-        std::cout << it->first << " => " << it->second[0] << " " <<it->second[1]<< " "
-         << it->second[2] << '\n';
-}
+  map<string, int[T_SIZE]> table;
 
-vector<string> symbol_table::search_not_defined(){
-    vector<string> not_defined;
-    for(itr=table.begin(); itr!= table.end(); itr++){
-        if(!itr->second[DEF]){
-            not_defined.insert(not_defined.end(), itr->first);
-        }
+  for (map<string, int[T_SIZE]>::iterator it= this->table.begin(); it!=this->table.end(); ++it)
+    std::cout << it->first << " => " << it->second[0] << " " <<it->second[1]<< " "
+      << it->second[2] << '\n';
+  }
+
+  vector<string> symbol_table::search_not_defined(){
+  vector<string> not_defined;
+  for(itr=table.begin(); itr!= table.end(); itr++){
+    if(!itr->second[DEF]){
+        not_defined.insert(not_defined.end(), itr->first);
     }
-    return not_defined;
+  }
+  return not_defined;
 }
 
 // CLASS LABEL_OCCURRENCE
 
 label_occurrence::label_occurrence(string label, string instruction_operator, string code_line, int line_number){
-    this->label = label;
-    this->instruction_operator = instruction_operator;
-    this->code_line = code_line;
-    this->line_number = line_number;
+  this->label = label;
+  this->instruction_operator = instruction_operator;
+  this->code_line = code_line;
+  this->line_number = line_number;
 }
 
 // CLASS ERROR
 
 map<int, string> error::error_details = {
-    {1000, "Declaração e rótulo ausente."},
-    {2000, "Declaração e rótulo repetido."},
-    {2001, "Pulo para rótulo inválido."},
-    {2002, "Pulo para seção errada."},
-    {1001, "Diretiva inválida."},
-    {1002, "Instrução inválida."},
-    {2003, "Diretiva ou instrução na seção errada."},
-    {2004, "Divisão por zero."},
-    {1003, "Instrução com a quantidade de operandos inválida."},
-    {1004, "Instrução com o tipo de operando inválido."},
-    {0000, "Token inválido."},
-    {1005, "Dois rótulos na mesma linha."},
-    {2005, "Seção TEXT faltante."},
-    {1006, "Seção inválida."},
-    {1007, "Tipo de argumento inválido."},
-    {2006, "Modificação de um valor constante."},
-    {2007, "Acesso a posição não reservada pelo SPACE."}
+  {1000, "Declaração e rótulo ausente."},
+  {2000, "Declaração e rótulo repetido."},
+  {2001, "Pulo para rótulo inválido."},
+  {2002, "Pulo para seção errada."},
+  {1001, "Diretiva inválida."},
+  {1002, "Instrução inválida."},
+  {2003, "Diretiva ou instrução na seção errada."},
+  {2004, "Divisão por zero."},
+  {1003, "Instrução com a quantidade de operandos inválida."},
+  {1004, "Instrução com o tipo de operando inválido."},
+  {0000, "Token inválido."},
+  {1005, "Dois rótulos na mesma linha."},
+  {2005, "Seção TEXT faltante."},
+  {1006, "Seção inválida."},
+  {1007, "Tipo de argumento inválido."},
+  {2006, "Modificação de um valor constante."},
+  {2007, "Acesso a posição não reservada pelo SPACE."}
 };
 
 map<int, string> error::error_types = {
-    {1, "Sintático"},
-    {2, "Semantico"},
-    {0, "Léxico"}
+  {1, "Sintático"},
+  {2, "Semantico"},
+  {0, "Léxico"}
 };
 
 
 error::error(string error_line, int error_line_number, int error_code){
-    this->error_line_number = error_line_number;
-    this->error_line = error_line;
-    this->error_code = error_code;
-    this->error_type = (int)(error_code/ERROR_TYPE_MASK);
+  this->error_line_number = error_line_number;
+  this->error_line = error_line;
+  this->error_code = error_code;
+  this->error_type = (int)(error_code/ERROR_TYPE_MASK);
 }
 
 // CLASS ERROR_LOG
 
 void error_log::display(int option){
-    if(error_list.empty()){return;}
-    cout << "Relatório de Erros:\n" << endl;
-    for(itr=error_list.begin(); itr!=error_list.end(); itr++){
-        cout << "Número da linha: " << itr->get_error_line_number() << "     Tipo: " << 
-        itr->error_types[itr->get_error_type()] << endl; 
-        if(option == DETAILED){
-            cout << "Linha de código: " << itr->get_error_line();
-            cout << "Detalhes: " << itr->get_error_details() << endl;
-        }
-        cout<<endl;
+  if(error_list.empty()){return;}
+  cout << "Relatório de Erros:\n" << endl;
+  for(itr=error_list.begin(); itr!=error_list.end(); itr++){
+    cout << "Número da linha: " << itr->get_error_line_number() << "     Tipo: " << 
+    itr->error_types[itr->get_error_type()] << endl; 
+    if(option == DETAILED){
+        cout << "Linha de código: " << itr->get_error_line();
+        cout << "Detalhes: " << itr->get_error_details() << endl;
     }
+    cout<<endl;
+  }
 }
